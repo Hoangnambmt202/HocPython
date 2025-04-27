750b1354e5a093d2633af68a65179a04c8568069
Hoang Nam <nam23062002@gmail.com>
2025-04-27 18:53:19 +0700

    fix bug cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 // Middleware
 // Trên server hocpython-backend.onrender.com
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://hocpython-backend.onrender.com'],
+  origin: ['http://localhost:5173', 'https://hoc-python.vercel.app/', 'https://hocpython-backend.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
