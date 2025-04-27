d7546d4d0d34f67beccd6ebf1fec1d7d45a2219e
Hoang Nam <nam23062002@gmail.com>
2025-04-27 19:44:11 +0700

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Cho phép tất cả các route
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
